90a60a11e6bc4cb45e3bc97280ecbc925d068427
FreemanLouie <freemanmlouie@gmail.com>
2025-04-26 02:19:32 -0700

    Update RandomStringChooser.java

--- src/main/java/RandomStringChooser.java
+++ src/main/java/RandomStringChooser.java
@@ -3,7 +3,7 @@ public class RandomStringChooser
 {
   /* to be implemented in part (a) */
   private ArrayList<String> aList = new ArrayList<String>();
-  RandomStringChooser(String list){
+  RandomStringChooser(String[] list){
     for(int i = 0; i < list.length; i++)
       aList.add(list[i]);
   }
